0ecf9332a3e5d45a59d1749f0c27b4f0a2c9eafd
Christian Brandalise <christian.brandalise@gmail.com>
2015-05-10 18:19:58 -0500

    wedding app ready for deployment

---
+++ server/public/scripts/app.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){
+	$('#guest-form').hide();
+	function fetch(){
+		$("#guest-table tbody").empty()
+		$.ajax({
+			type: "GET",
+			contentType: 'application/json',
+			url: 'http://localhost:3000/api/guests',
+			success: function(results){
+				var filter = filterData(results)
+				appendToNavbar(filter);
+				results.forEach(function(guest){
+					appendToTable(guest.id, guest);
+				})
+			}
+		})	
+	}
+
+	fetch();
+	
+	//TOGGLE ADD NEW GUEST FORM.
+	$('#new-guest-btn').on('click', function(){
+		$('#guest-form').slideToggle();
+	})
+
+
+	//SUBMIT FORM DATA TO DATABASE
+	$("form").submit(function(evnt){
+		evnt.preventDefault()
+		var guestData = {
+			name: $("#nome").val(),
+			address: $("#endereco").val(),
+			email: $("#email").val(),
+			phone: $("#phone").val(),
+			guests: $("#convidados").val(),
+			confirmation: $("#presenca").val().toUpperCase()
+		}
+
+		$.ajax({
+			type: "POST",
+			contentType: 'application/json',
+			url: 'http://localhost:3000/api/guests',
+			data: JSON.stringify(guestData),
+			success: function(result){
+				//append data to table immediatelly
+				appendToTable(result.id, guestData);
+				fetch();
+			},
+			error: function(err){
+				console.log("shit happened!", err)
+			}
+		})
+
+		//Remove entry from table
+
+		//reset form values and hide form after sumbission
+    	$("#nome").val("");
+		$("#endereco").val("");
+		$("#email").val("");
+		$("#phone").val("");
+		$("#convidados").val("");
+		$("#presenca").val("");
+		$("#guest-form").fadeOut();
+	})
+
+	$(document).on('click', '#delete', function(evnt){
+		evnt.preventDefault()
+		var id = $(this).closest("tr").attr('id');
+		console.log("ID:", id);
+		$(this).closest("tr").remove();
+	})
+	///////////////////////////////////////////////////////////////
+	//                       HELPERS                             //
+	//////////////////////////////////////////////////////////////
+	
+	function appendToTable(id, guestData){
+		var context = {
+			"SIM": "success",
+			"NAO": "danger",
+			"TALVEZ": "warning"
+		}
+		$("#guest-table tbody").append(
+		'<tr class="'+ context[guestData["confirmation"]]+ '">' +
+          "<td id='id'>" + id + "</td>" +
+          "<td>" + guestData["name"] + "</td>" +
+          "<td>" + guestData["address"] +"</td>" +
+          "<td>" + guestData["email"] + "</td>" +
+          "<td>" + guestData["phone"] + "</td>" +
+          "<td>" + guestData["guests"] + "</td>" +
+          "<td>" + guestData["confirmation"] + "</td>" +
+          '<td id="delete"><span class="glyphicon glyphicon-trash red"></span></td>' +
+        "</tr>").slideDown("slow");
+	}
+
+	//Filters based on yes no and maybe
+	function filterData(results){
+		var confirmation = {
+			length: results.length,
+			yes: [],
+			no: [],
+			maybe:[]
+		}
+		
+		results.forEach(function(guest){
+			if(guest.confirmation === "SIM"){
+				confirmation.yes.push(guest)
+			} else if(guest.confirmation === "NAO"){
+				confirmation.no.push(guest)
+			} else {
+				confirmation.maybe.push(guest)
+			}
+		})
+		return confirmation
+	}
+
+	//appends filtered results to navbar
+	function appendToNavbar(results){
+		$('#total span').text(results.length)
+		$('#count-sim span').text(results.yes.length)
+		$('#count-nao span').text(results.no.length)
+		$('#count-talvez span').text(results.maybe.length)
+	}
+})
---
+++ server/server.js
@@ -0,0 +1,150 @@
+var express = require('express');
+var app = express();
+var pg = require('pg');
+var bodyParser = require('body-parser')
+var session = require('express-session')
+
+/////////////////////////////////////////////////////////////////////
+//                          DB CONNECTION                          //
+/////////////////////////////////////////////////////////////////////
+
+// var connectionString = "postgres://gwlpywdbzjuuyv:yWhO_BIPji1cc513rEtpwt7cY7@ec2-54-225-157-157.compute-1.amazonaws.com:5432/d41ri138f8kobu"
+
+var db = new pg.Client({
+	user: "gwlpywdbzjuuyv",
+    password: "yWhO_BIPji1cc513rEtpwt7cY7",
+    database: "d41ri138f8kobu",
+    port: 5432,
+    host: "ec2-54-225-157-157.compute-1.amazonaws.com",
+    ssl: true
+});
+function connectDatabase(callback){
+	db.connect(function(err){
+		if(err){
+			console.log("PG CONNECTION ERROR:", err);
+		} else {
+			console.log("CONNECTED TO WEDDING-DB POSTGRES DB");
+			callback();
+		}
+	})
+}
+
+var users = 'CREATE TABLE IF NOT EXISTS users (' +
+			'id SERIAL NOT NULL PRIMARY KEY,'    +
+			'email VARCHAR(255) NOT NULL,'       +
+			'hash VARCHAR(255) NOT NULL'         +
+			');';
+
+var guests = 'CREATE TABLE IF NOT EXISTS guests (' +
+			 'id SERIAL NOT NULL PRIMARY KEY,'     +
+			 'name VARCHAR(125) NOT NULL,'		   +
+			 'address VARCHAR(255),'			   +
+			 'email VARCHAR(255),'                 +
+			 'phone VARCHAR(255),'                 +
+			 'guests INTEGER,'					   +
+			 'confirmation VARCHAR(10) NOT NULL'   +
+			 ');';
+
+connectDatabase(function(){
+	db.query(users, function(err, results){
+		if(err) console.log("users table creation ERROR:", err)
+		console.log("created users table.");
+	});
+
+	db.query(guests, function(err, results){
+		if(err) console.log("guests table creation ERROR:", err)
+		console.log("created guests table.");
+	});
+});
+
+/////////////////////////////////////////////////////////////////////
+//                          MIDDLEWARE                             //
+/////////////////////////////////////////////////////////////////////
+
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.use(session({
+  secret: '18012013',
+  resave: false,
+  saveUninitialized: true
+}))
+
+/////////////////////////////////////////////////////////////////////
+//                          ROUTES                                 //
+/////////////////////////////////////////////////////////////////////
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/views/login.html')
+})
+
+app.get('/main', function(req, res){
+	if(req.session.email){
+		res.sendFile(__dirname + '/views/index.html')	
+	} else {
+		res.redirect('/');
+	}
+})
+
+app.post('/api/auth', function(req, res){
+	console.log(req.body.email, req.body.hash)
+	db.query("SELECT * FROM users WHERE email = $1 AND hash = $2", [req.body.email, req.body.hash], function(err, result){
+		if(err){ console.log("ERROR", err) }
+		else {
+			if(result.rows && result.rows.length > 0){
+				req.session.email = result.rows[0].email;
+				console.log('req.session.email:', req.session.email)
+				res.redirect('/main');
+			} else {
+				res.redirect('/');
+			}
+		}
+	})
+})
+
+app.get('/api/auth/logout', function(req, res){
+	console.log(req.session.email)
+	if(req.session.email){
+		req.session.destroy(function(err){
+			if(err) { console.log("SESSION ERROR:", err) }
+			else { console.log("Session destroyed") }
+		})
+		res.redirect('/')
+	} else {
+		res.send("nothing to see here!")
+	}
+})
+
+app.get('/api/guests', function(req, res){
+	if(req.session.email){
+		db.query("SELECT * FROM guests", function(err, result){
+			if(result && result.rows.length > 0){
+				res.status(200).json(result.rows)
+			} else {
+				res.status(404).json({
+					message: "ERROR - Nothing found"
+				})
+			}
+		})		
+	} else {
+		res.status(401).json({
+			message: 'not authorized!'
+		})
+	}
+})
+
+app.post('/api/guests', function(req, res){
+	if(req.session.email){
+		db.query("INSERT INTO guests (name, address, email, phone, guests, confirmation) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id", 
+		[req.body.name, req.body.address, req.body.email, req.body.phone, req.body.guests, req.body.confirmation], function(err, result){
+			res.status(201).json(result.rows[0])
+		})
+	} else {
+		res.status(401).json({
+			message: 'not authorized!'
+		})
+	}
+})
+
+app.listen(3000, function(){
+	console.log("Server listening on PORT: 3000");
+})
